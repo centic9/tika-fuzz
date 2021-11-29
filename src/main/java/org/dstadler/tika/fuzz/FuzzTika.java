6d0aa67195a2da35cb90facbb08d75d0b45db38d
Dominik Stadler <dominik.stadler@gmx.at>
2021-11-29 08:03:08 +0100

    Switch to Tika 2.0.0 for fuzzing
    Add most of the available parsers

--- src/main/java/org/dstadler/tika/fuzz/FuzzTika.java
+++ src/main/java/org/dstadler/tika/fuzz/FuzzTika.java
@@ -2,18 +2,25 @@ package org.dstadler.tika.fuzz;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.apache.tika.Tika;
-import org.apache.tika.io.TikaInputStream;
 
 public class FuzzTika {
 	public static void fuzzerTestOneInput(byte[] input) {
 		final Tika tika = new Tika();
 
 		// try to invoke various methods which parse documents/workbooks/slide-shows/...
-		try (TikaInputStream str = TikaInputStream.get(new ByteArrayInputStream(input))) {
-			tika.detect(str, "fuzzed input");
-		} catch (IOException e) {
+
+		try (InputStream str = new ByteArrayInputStream(input)) {
+			tika.detect(str);
+		} catch (IOException | IllegalArgumentException e) {
+			// expected here
+		}
+
+		try (InputStream str = new ByteArrayInputStream(input)) {
+			tika.parse(str);
+		} catch (IOException | IllegalArgumentException e) {
 			// expected here
 		}
 	}
