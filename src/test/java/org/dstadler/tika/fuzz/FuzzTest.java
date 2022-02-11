5446f24c75f8b9e31e7e002d2655a0366016ed17
Dominik Stadler <dominik.stadler@gmx.at>
2022-02-11 08:47:08 +0100

    Add a disabled test to allow to run reported "slow-unit" cases

--- src/test/java/org/dstadler/tika/fuzz/FuzzTest.java
+++ src/test/java/org/dstadler/tika/fuzz/FuzzTest.java
@@ -1,5 +1,10 @@
 package org.dstadler.tika.fuzz;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class FuzzTest {
@@ -8,5 +13,11 @@ class FuzzTest {
 		Fuzz.fuzzerTestOneInput(new byte[] {});
 		Fuzz.fuzzerTestOneInput(new byte[] {1});
 		Fuzz.fuzzerTestOneInput(new byte[] {'P', 'K'});
+	}
+
+	@Disabled("Local test for verifying a slow run")
+	@Test
+	public void testSlowUnit() throws IOException {
+		Fuzz.fuzzerTestOneInput(FileUtils.readFileToByteArray(new File("slow-unit-9b30ff4d9f28e68033cfe554a0ee373d5ef4f0ca")));
 	}
 }
