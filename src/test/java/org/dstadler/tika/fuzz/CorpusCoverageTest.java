bd86aa80702d56d32f1f60a62238e1601ea009c0
Dominik Stadler <dominik.stadler@gmx.at>
2022-02-24 22:43:20 +0100

    Add a disabled test which can be used to process all current corpus files

---
+++ src/test/java/org/dstadler/tika/fuzz/CorpusCoverageTest.java
@@ -0,0 +1,58 @@
+package org.dstadler.tika.fuzz;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.NameFileFilter;
+import org.apache.commons.io.filefilter.NotFileFilter;
+import org.apache.commons.io.filefilter.OrFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * A simple test which runs all files from the corpus to be
+ * able to see where we are missing coverage, i.e. which types
+ * of corpus files are missing.
+ *
+ * Disabled as it can run for a long time.
+ */
+@Execution(ExecutionMode.CONCURRENT)
+public class CorpusCoverageTest {
+	private static final Set<String> EXCLUDED = Set.of();
+
+	@Disabled("Can run a long time when there is a large corpus used for fuzzing")
+	@ParameterizedTest
+	@MethodSource("provideStringsForIsBlank")
+	void testCorpusFile(File file) throws IOException {
+		System.err.println("Running file " + file + " in thread " + Thread.currentThread().getName());
+		Fuzz.fuzzerTestOneInput(FileUtils.readFileToByteArray(file));
+	}
+
+	private static Stream<Arguments> provideStringsForIsBlank() {
+		Collection<File> files = FileUtils.listFiles(new File("corpus"),
+				// all files
+				TrueFileFilter.TRUE,
+				// but exclude ".svn" or ".git" directory
+				new NotFileFilter(
+						new OrFileFilter(
+							new NameFileFilter(".svn"),
+							new NameFileFilter(".git")
+						)
+				));
+
+		// wrap in TreeSet to have a sorted list
+		return new TreeSet<>(files).stream()
+				.filter(file -> !EXCLUDED.contains(file.getName()))
+				.map(Arguments::of);
+	}
+}
