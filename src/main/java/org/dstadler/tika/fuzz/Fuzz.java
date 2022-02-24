055bed9870eaf8c700db5ad60f081c781ac68a82
Dominik Stadler <dominik.stadler@gmx.at>
2022-02-24 22:42:45 +0100

    Read and close a successfully created Reader
    Otherwise fuzzing accumulates more and more Threads which
    are all waiting for processing of the data and thus
    causes exhaustion of the SAXParser-pool

--- src/main/java/org/dstadler/tika/fuzz/Fuzz.java
+++ src/main/java/org/dstadler/tika/fuzz/Fuzz.java
@@ -3,6 +3,7 @@ package org.dstadler.tika.fuzz;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Reader;
 
 import org.apache.tika.Tika;
 
@@ -10,19 +11,28 @@ public class Fuzz {
 	private static final Tika tika = new Tika();
 
 	public static void fuzzerTestOneInput(byte[] input) {
-
 		// try to invoke various methods which parse documents/workbooks/slide-shows/...
 
 		try (InputStream str = new ByteArrayInputStream(input)) {
 			tika.detect(str);
 		} catch (IOException | IllegalArgumentException e) {
-			// expected here
+			// expected here, Tika throws both types of exceptions here
 		}
 
 		try (InputStream str = new ByteArrayInputStream(input)) {
-			tika.parse(str);
+			try (Reader reader = tika.parse(str)) {
+				char[] bytes = new char[1024];
+
+				// make sure to read all the resulting data
+				while (true) {
+					int read = reader.read(bytes);
+					if (read == -1) {
+						break;
+					}
+				}
+			}
 		} catch (IOException | IllegalArgumentException e) {
-			// expected here
+			// expected here, Tika throws both types of exceptions here
 		}
 	}
 }
